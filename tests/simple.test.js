ab9700f2ba9cfb6e9fd8e5d499b02ac094176c20
ZigGreen <krisstal.mail@gmail.com>
2018-04-28 14:54:32 +0300

    tests rewrite

--- tests/simple.test.js
+++ tests/simple.test.js
@@ -2,5 +2,5 @@ const vim = require('../src/vim').vim;
 const { BS } = require('cr-event-tools').specialKeys;
 
 test('simple', () => {
-    expect(vim).runsCommand(`ihello Worldi${BS}`);
+    expect(vim(`ihello Worldi${BS}`).str).toBe('hello World');
 });
